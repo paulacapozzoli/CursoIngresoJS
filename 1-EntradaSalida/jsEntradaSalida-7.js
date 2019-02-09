60e72999290232d34c6ef0205510746236aa3663
Paula <pcapozzoli.gcba@gmail.com>
2019-02-08 21:46:23 -0300

    ejercicio 7 terminado

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -2,23 +2,78 @@
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
+
+//numeroUno
+//numeroDos
+
 function sumar()
 {	
-	
+	var primerNumero;
+	var segundoNumero;
+	var suma;
+
+	primerNumero=numeroUno.value;
+	segundoNumero=numeroDos.value;
+
+	primerNumero=parseInt(primerNumero);
+	segundoNumero=parseInt(segundoNumero);
+
+	suma=primerNumero+segundoNumero;
+
+	alert ("El resultado es "+suma);
+
 }
 
 function restar()
 {
+	var primerNumero;
+	var segundoNumero;
+	var resta;
+
+	primerNumero=numeroUno.value;
+	segundoNumero=numeroDos.value;
+
+	primerNumero=parseInt(primerNumero);
+	segundoNumero=parseInt(segundoNumero);
+
+	resta=primerNumero-segundoNumero;
+
+	alert ("El resultado es "+resta);
 	
 }
 
 function multiplicar()
 { 
-	
+	var primerNumero;
+	var segundoNumero;
+	var producto;
+
+	primerNumero=numeroUno.value;
+	segundoNumero=numeroDos.value;
+
+	primerNumero=parseInt(primerNumero);
+	segundoNumero=parseInt(segundoNumero);
+
+	producto=primerNumero*segundoNumero;
+
+	alert("El resultado es "+producto);
 }
 
 function dividir()
 {
+	var primerNumero;
+	var segundoNumero;
+	var cociente;
+
+	primerNumero=numeroUno.value;
+	segundoNumero=numeroDos.value;
+
+	primerNumero=parseInt(primerNumero);
+	segundoNumero=parseInt(segundoNumero);
+
+	cociente=primerNumero/segundoNumero;
+
+	alert ("El resultadoes "+cociente);
 	
 }
 
