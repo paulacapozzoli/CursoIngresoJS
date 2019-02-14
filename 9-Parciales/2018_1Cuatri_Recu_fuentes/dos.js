e58b979e6d8c32289987f24468500cbb0a60523d
alumno <alumno>
2019-02-14 19:49:26 -0300

    ejercicio 2 terminado

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
@@ -1,4 +1,23 @@
 function mostrar()
 {
   
+  var importe;
+  var importeDesc;
+  var importeFinal;
+  var descuento;
+  var iva;
+
+  importe=prompt("Ingrese el valor de la compra");
+
+  importe=parseInt(importe);
+  
+  descuento=importe*10/100;
+
+  importeDesc=importe-descuento;
+
+  iva=importeDesc*21/100;
+
+  importeFinal=importeDesc+iva;
+
+  alert ("tu compra es de $"+importe+" tenes un descuento del 10% queda en $"+importeDesc+", mas el iva es $"+importeFinal);
 }
