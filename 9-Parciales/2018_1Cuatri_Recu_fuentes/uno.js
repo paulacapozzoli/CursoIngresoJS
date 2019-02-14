e22dc24b78bd7df747baf01172828b7d8b3f371e
alumno <alumno>
2019-02-14 19:37:14 -0300

    ejercicio 1 terminado

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,10 +1,21 @@
 
 function mostrar()
 {
-	var pesoThor;
-	var pesoCali;
+	var nombreUno;
+	var nombreDos;
+	var pesoUno;
+	var pesoDos;
 	var suma;
 
-	alert ("tenes dos mascotas Thor y Cali, que pesan "++" y "++" kilos, la suma de los kilos es "+)
+	var nombreUno=prompt("Ingrese el nombre de la primer mascota");
+	var nombreDos=prompt("Ingrese el nombre de la segunda mascota");
+	var pesoUno=prompt("Ingrese el peso de la primer mascota");
+	var pesoDos=prompt("Ingrese el peso de la primer mascota");
 
+	pesoUno=parseInt(pesoUno);
+	pesoDos=parseInt(pesoDos);
+
+	suma=pesoUno+pesoDos;
+
+	alert ("tenes dos mascotas "+nombreUno+" y "+nombreDos+" que pesan "+pesoUno+" y "+pesoDos+" kilos, la suma de los kilos es: "+suma);
 }
