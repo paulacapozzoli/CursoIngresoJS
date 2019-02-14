f7fe7349991b1349e5a81d5f96bebb2960c06dd8
alumno <alumno>
2019-02-14 20:07:55 -0300

    ejercicio 3 terminado

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,28 @@
 function mostrar()
 {
+	var subTotal;
+	var iva;
+	var importeIva
+	var propina;
+	var importeFinal;
+	var comensales;
+	var importeComensales;
+
+	subTotal=prompt("Ingrese el sub total en $");
+	comensales=prompt("Ingrese la cantidad de comensales");
+
+	subTotal=parseInt(subTotal);
+	comensales=parseInt(comensales);
+
+	iva=subTotal*21/100;
+	importeIva=subTotal+iva;
+
+	propina=importeIva*10/100;
+	importeFinal=importeIva+propina;
+
+	importeComensales=importeFinal/comensales;
+
+	alert("El sub total es $"+subTotal+" mas iva es $"+importeIva+" mas la propina del 10% es $"+importeFinal+" Cada comensal abona $"+importeComensales);
+
 
 }
