ae7d210c7a0839bfa571dd4b5bcc8c4ad56b76af
alumno <alumno>
2019-02-15 19:08:38 -0300

    ejercicio 9 y 10

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,30 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
+	var numeroRandom;
+	var mensaje;
+
+	numeroRandom=Math.floor((Math.random ()*10) +1);
+	console.log(numeroRandom);//Genero el número RANDOM entre 1 y 10 
 	
+	if (numeroRandom<11 && numeroRandom>8)
+	{
+		mensaje="EXCELENTE"
+	}
+
+	else
+	{
+		if (numeroRandom<9 && numeroRandom>3)
+		{
+			mensaje="APROBO"
+		}
+
+		if (numeroRandom<4)
+		{
+			mensaje="Vamos, la proxima se puede";
+
+			}
+	}
+
+	alert("Su nota es: "+numeroRandom+" "+ mensaje);
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,8 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var numeroRandom;
+	numeroRandom=Math.floor((Math.random ()*10) +1);//lo que hago es correr la coma un lugar y sumarle un entero
+	console.log(numeroRandom);
 
 }//FIN DE LA FUNCIÓN
