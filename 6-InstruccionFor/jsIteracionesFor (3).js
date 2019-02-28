51d8c4b474064dfa16299cbe3ee9adb571a19d15
alumno <alumno>
2019-02-28 20:23:45 -0300

    Nros Perfectos/Primos FOR

--- 6-InstruccionFor/jsIteracionesFor (3).js
+++ 6-InstruccionFor/jsIteracionesFor (3).js
@@ -2,7 +2,36 @@ function mostrar()
 {
 
 var repetciones = prompt("ingrese el número de repeticiones");
+var numeroIngresado;
+var numeroAnterior;
+var 
+
+numeroIngresado=prompt("Ingrese un número");
+numeroIngresado=parseInt(numeroIngresado);
+
+for(numeroAnterior=numeroIngresado-1;numeroAnterior>0;numeroAnterior--) 
+	{
+	console.log(numeroAnterior);
+
+	if (numeroIngresado%numeroAnterior==0)//quiere decir que es divisible 
+		{
+			console.log(numeroAnterior);
+			acumulador= acumulador+numeroAnterior;			
+		}
+
+	
+	}
+
+if(numeroIngresado==acumulador) 
+	{
+		console.log("Es perfecto");
+	}
+
+else
+	{
+		console.log("No es perfecto");
+	}
+}
 
 
 
-}//FIN DE LA FUNCIÓN
--- 6-InstruccionFor/jsIteracionesFor (4).js
+++ 6-InstruccionFor/jsIteracionesFor (4).js
@@ -1,7 +1,29 @@
 function mostrar()
 {
+	var numeroIngresado;
+	var numeroAnterior;
+	var numeroRecorrido;
 
+	numeroIngresado=prompt("Ingrese un número");
+	numeroIngresado=parseInt(numeroIngresado);
 
+	for (numeroRecorrido=numeroIngresado-1;numeroRecorrido>1;numeroRecorrido--)
+	{
 
+		for(numeroAnterior=numeroRecorrido-1;numeroAnterior>1;numeroAnterior--)
+		{
+			if(numeroRecorrido % numeroAnterior == 0)
+			{
+				break;
+			}
+		}
+
+		if (numeroAnterior ==1 ) 
+		{
+			console.log("Es primo"+numeroRecorrido);
+		}
+	}
+
+	
 
 }//FIN DE LA FUNCIÓN
