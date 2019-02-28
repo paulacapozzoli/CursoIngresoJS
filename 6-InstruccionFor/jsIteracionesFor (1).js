a6962cc993c8c25693a8d4b9959c5277aaa7bf31
alumno <alumno>
2019-02-28 19:51:29 -0300

    nros primos 2 FOR

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,4 +1,12 @@
 function mostrar()
 {
+	var contador;
+
+	contador=0;
+
+	for (contador=0;contador<5;contador++) 
+	{
+		console.log(contador);
+	}
 
 }
--- 6-InstruccionFor/jsIteracionesFor (2).js
+++ 6-InstruccionFor/jsIteracionesFor (2).js
@@ -1,5 +1,31 @@
 function mostrar()
 {
+	var numeroIngresado;
+	var numeroAnterior;
 
+	numeroIngresado=prompt("Ingrese un número");
+	numeroIngresado=parseInt(numeroIngresado);
 
+	for(numeroAnterior=numeroIngresado-1;numeroAnterior>0;numeroAnterior--) 
+	{
+		console.log(numeroAnterior);
+
+		if (numeroIngresado%numeroAnterior==0)//quiere decir que es divisible 
+		{
+			break; //no hace falta armar un contador de divisores por eso break
+		}
+
+		console.log(numeroAnterior);
+	}
+
+	if(numeroAnterior==1) 
+	{
+		console.log("Es primo");	
+	}
+
+	else
+	{
+		console.log("No es primo");
+	}
 }
+
